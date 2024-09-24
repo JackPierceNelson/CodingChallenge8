adf741caf77d2fb9771f0155a75bb55576129af6
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-09-24 13:02:35 -0500

    Create Manager class with inheritance from Employee

--- employee_management.js
+++ employee_management.js
@@ -19,16 +19,20 @@ console.log(bob.getDetails());
 
 // Task 2: Create a Department Class
 class Department {
+    // initializing the department with a name and an empty array for employees
     constructor(name) {
         this.name = name;
         this.employees = [];
     }
+    // add an employee object to the employees array
     addEmployee(employee) {
         if (employee instanceof Employee) {
             this.employees.push(employee);
         }
+        // checking to ensure the valid employee objects are added
         else {console.error("Invalid employee object");}
     }
+    // returns the total salary of all employees in the department and reduce method would sum up all employee salaries
     getDepartmentSalary() {
         return this.employees.reduce((total, employee) => total + employee.salary, 0); 
     }
@@ -37,4 +41,25 @@ class Department {
 const Engineering = new Department("Engineering");
 Engineering.addEmployee("Jack");
 Engineering.addEmployee(alice);
+// calculating department salaries
 console.log(`Engineering department total Salary: $${Engineering.getDepartmentSalary()}`);
+
+// Task 3: Create a Manager Class that Inherits from Employee
+class Manager extends Employee {
+    // Used super to call the employee constructor with base properties
+    constructor(name, salary, position, department, bonus) {
+        super(name, salary, position, department); 
+        // adding the bonus property specific to managers
+        this.bonus = bonus;
+    }
+    // overiding the getDetails method in the parent class using super.getDetails
+    // appending the bonus info to the string for the manager
+    getDetails() {
+        return `${super.getDetails()} - Bonus: $${this.bonus}`;
+    }
+
+}
+// test case
+const robert = new Manager("Robert", 300000, "Lead Manager", "Engineering", 20000);
+console.log(robert.getDetails());
+
