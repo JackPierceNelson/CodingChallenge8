9637a534278a1628e8aab8723ebe65f3f18288e9
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-09-24 12:48:15 -0500

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -1,5 +1,5 @@
 // Task 1: Create an Employee Class
-class employee {
+class Employee {
     constructor( name, salary, position, department) {
         this.name = name;
         this.salary = salary;
@@ -11,8 +11,30 @@ class employee {
     }
 }
 // create employees
-const alice = new employee("Alice", 80000, "Developer", "Engineering");
-const bob = new employee("Bob", 75000, "Designer", "Marketing");
+const alice = new Employee("Alice", 80000, "Developer", "Engineering");
+const bob = new Employee("Bob", 75000, "Designer", "Marketing");
 // extracting employee details
 console.log(alice.getDetails());
 console.log(bob.getDetails());
+
+// Task 2: Create a Department Class
+class Department {
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee) {
+        if (employee instanceof Employee) {
+            this.employees.push(employee);
+        }
+        else {console.error("Invalid employee object");}
+    }
+    getDepartmentSalary() {
+        return this.employees.reduce((total, employee) => total + employee.salary, 0); 
+    }
+}
+// add employees to department
+const Engineering = new Department("Engineering");
+Engineering.addEmployee("Jack");
+Engineering.addEmployee(alice);
+console.log(`Engineering department total Salary: $${Engineering.getDepartmentSalary()}`);
